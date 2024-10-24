ed8b1f9691c4c33abe48105eecf3168154a69c9a
elyse502 <elyseniyibizi502@gmail.com>
2024-10-24 15:24:02 +0200

    Add Hero Section for the Home Page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,11 +1,12 @@
-import React from 'react'
+import React from "react";
+import Hero from "../components/Hero";
 
 const Home = () => {
   return (
     <div>
-      Home
+      <Hero />
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
