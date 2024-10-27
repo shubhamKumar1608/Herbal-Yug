f95465be00592aced8a54850416e869676a8c43d
Elysée NIYIBIZI <elyseniyibizi502@gmail.com>
2024-10-28 00:53:30 +0100

    Make Search Feature from the Collection Page Functional

--- frontend/src/components/SearchBar.jsx
+++ frontend/src/components/SearchBar.jsx
@@ -4,7 +4,7 @@ import { assets } from "../assets/assets";
 import { useLocation } from "react-router-dom";
 
 const SearchBar = () => {
-  const { search, setsearch, showSearch, setShowSearch } =
+  const { search, setSearch, showSearch, setShowSearch } =
     useContext(ShopContext);
   const [visible, setVisible] = useState(false);
   const location = useLocation();
@@ -22,7 +22,7 @@ const SearchBar = () => {
       <div className="inline-flex items-center justify-center border border-gray-400 px-5 py-2 my-5 mx-3 rounded-full w-3/4 sm:w-1/2">
         <input
           value={search}
-          onChange={(e) => setShowSearch(e.target.value)}
+          onChange={(e) => setSearch(e.target.value)}
           className="flex-1 outline-none bg-inherit text-sm"
           type="text"
           placeholder="Search"
