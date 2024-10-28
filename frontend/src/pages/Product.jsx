c24e1355cc26125f974ef8b139a2f1e11423a042
elyse502 <elyseniyibizi502@gmail.com>
2024-10-28 20:39:10 +0200

    Add Description and Reviews Section to the Product page

--- frontend/src/pages/Product.jsx
+++ frontend/src/pages/Product.jsx
@@ -97,6 +97,32 @@ const Product = () => {
           </div>
         </div>
       </div>
+
+      {/* ------------- Description & Review Section ------------- */}
+      <div className="mt-20">
+        <div className="flex">
+          <b className="border px-5 py-3 text-sm">Description</b>
+          <p className="border px-5 py-3 text-sm">Reviews (122)</p>
+        </div>
+        <div className="flex flex-col gap-4 border px-6 py-6 text-sm text-gray-500">
+          <p>
+            {" "}
+            An e-commerse website is an online platform that facilitates the
+            buying and selling of products or services over the internet. It
+            serves as a virtual marketplace where businesses and individuals can
+            showcase their products, interact with customers, and conduct
+            transactions without the need for a physical presence. E-commerce
+            websites have gained immense popularity due to their convenience,
+            accessibility, and the global reach they offer.
+          </p>
+          <p>
+            E-commerce websites typically display products or services along
+            with detailed descriptions, images, prices, and any available
+            variations (e.g., sizes, colors). Each product usually has its own
+            dedicated page with relevant information.
+          </p>
+        </div>
+      </div>
     </div>
   ) : (
     <div className="opacity-0"></div>
