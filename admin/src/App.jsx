0fcb6d8217970ba2669ef0ef3fa237980b002576
elyse502 <elyseniyibizi502@gmail.com>
2024-11-04 18:54:21 +0200

    Create Admin dashboard

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,11 +1,37 @@
-import React from 'react'
+import React, { useState } from "react";
+import Navbar from "./components/Navbar";
+import Sidebar from "./components/Sidebar";
+import { Routes, Route } from "react-router-dom";
+import Add from "./pages/Add";
+import List from "./pages/List";
+import Orders from "./pages/Orders";
+import Login from "./components/Login";
 
 const App = () => {
-  return (
-    <div className='text-3xl font-bold underline'>
-      Hello world!
-    </div>
-  )
-}
+  const [token, setToken] = useState("");
 
-export default App
+  return (
+    <div className="bg-gray-50 min-h-screen">
+      {token === "" ? (
+        <Login />
+      ) : (
+        <>
+          <Navbar />
+          <hr />
+          <div className="flex w-full">
+            <Sidebar />
+            <div className="w-[70%] mx-auto ml-[max(5vw,25px)] my-8 text-gray-600 text-base">
+              <Routes>
+                <Route path="/add" element={<Add />} />
+                <Route path="/list" element={<List />} />
+                <Route path="/orders" element={<Orders />} />
+              </Routes>
+            </div>
+          </div>
+        </>
+      )}
+    </div>
+  );
+};
+
+export default App;
