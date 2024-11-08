db1da924055ef0ab900015801bdac45a379d7ed6
elyse502 <elyseniyibizi502@gmail.com>
2024-11-08 18:11:29 +0200

    Create Place Order APIs

---
+++ backend/routes/orderRoute.js
@@ -0,0 +1,25 @@
+import express from "express";
+import {
+  placeOrder,
+  placeOrderStripe,
+  allOrders,
+  userOrders,
+  updateStatus,
+} from "../controllers/orderController.js";
+import adminAuth from "../middleware/adminAuth.js";
+import authUser from "../middleware/auth.js";
+
+const orderRouter = express.Router();
+
+// Admin Features
+orderRouter.post("/list", adminAuth, allOrders);
+orderRouter.post("/status", adminAuth, updateStatus);
+
+// Payment Features
+orderRouter.post("/place", authUser, placeOrder);
+orderRouter.post("/stripe", authUser, placeOrderStripe);
+
+// user Feature
+orderRouter.post("/userorders", authUser, userOrders);
+
+export default orderRouter;
