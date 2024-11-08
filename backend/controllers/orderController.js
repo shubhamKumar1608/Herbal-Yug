d3dd41f2c9259fdb4b179cba15a6a3aca6a5e1f7
elyse502 <elyseniyibizi502@gmail.com>
2024-11-08 21:12:47 +0200

    Integrate User Orders API with the Frontend (Display placed Orders to the UI)

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -35,7 +35,17 @@ const placeOrderStripe = async (req, res) => {};
 const allOrders = async (req, res) => {};
 
 // User Order Data for Frontend
-const userOrders = async (req, res) => {};
+const userOrders = async (req, res) => {
+  try {
+    const { userId } = req.body;
+
+    const orders = await orderModel.find({ userId });
+    res.json({ success: true, orders });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+};
 
 // update order status from Admin Panel
 const updateStatus = async (req, res) => {};
