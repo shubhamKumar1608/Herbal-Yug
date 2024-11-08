d726d7745235a9d519645811628629a5b1305f92
elyse502 <elyseniyibizi502@gmail.com>
2024-11-08 19:04:01 +0200

    Fix some typos

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -1,5 +1,5 @@
 import orderModel from "../models/orderModel.js";
-import userModel from "../models/userModel";
+import userModel from "../models/userModel.js";
 
 // Placing orders using COD Method
 const placeOrder = async (req, res) => {
@@ -40,11 +40,4 @@ const userOrders = async (req, res) => {};
 // update order status from Admin Panel
 const updateStatus = async (req, res) => {};
 
-export {
-  placeOrder,
-  placeOrderStripe,
-  placeOrderRazorpay,
-  allOrders,
-  userOrders,
-  updateStatus,
-};
+export { placeOrder, placeOrderStripe, allOrders, userOrders, updateStatus };
