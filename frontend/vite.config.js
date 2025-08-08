6489952de58b03c5975b4be63dec6fa46064d9c5
Shubham Kumar <71170228+shubhamKumar1608@users.noreply.github.com>
2025-08-08 21:32:52 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,17 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   server: { port: 5173 },
+// });
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
 export default defineConfig({
   plugins: [react()],
-  server: { port: 5173 },
+  base: '/Herbal-Yug/', // ← Important: your repo name here
 });
+
